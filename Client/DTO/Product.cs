25255dae65d9a4d156f2bd6b900578abe2a25d43
Javier Hernandez <javier.hernandez@ntsprint.com>
2023-01-30 20:20:05 -0500

    Add project files.

---
+++ Client/DTO/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorShpoRadzen.Client.DTO
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; } 
+        public string ProductId { get; set; }   = Guid.NewGuid().ToString();
+        public double Price { get; set; }
+        public string image { get; set; }
+    }
+}
